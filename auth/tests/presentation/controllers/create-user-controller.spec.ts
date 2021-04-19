1b77188ea0e3e363f28bfa95f7c368f8db23f307
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-19 14:22:18 -0300

    test: ensure CreateUserController calls CreateUser with correct values

--- auth/tests/presentation/controllers/create-user-controller.spec.ts
+++ auth/tests/presentation/controllers/create-user-controller.spec.ts
@@ -1,20 +1,24 @@
 import { CreateUserController } from '@/presentation/controllers/create-user-controller'
 import { Validation, HttpRequest } from '@/presentation/protocols'
 import { badRequest } from '@/presentation/helpers'
-import { mockValidation, mockValidationFailure } from '@/tests/presentation/mocks'
+import { mockCreateUser, mockValidation, mockValidationFailure } from '@/tests/presentation/mocks'
 import { mockCreateUserParams } from '@/tests/domain/mocks'
+import { CreateUser } from '@/domain/usecases/user'
 
 type SutTypes = {
   sut: CreateUserController
   validationStub: Validation
+  createUserStub: CreateUser
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidation()
-  const sut = new CreateUserController(validationStub)
+  const createUserStub = mockCreateUser()
+  const sut = new CreateUserController(validationStub, createUserStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    createUserStub
   }
 }
 
@@ -25,18 +29,25 @@ const mockHttpRequest = (): HttpRequest => ({
 })
 
 describe('CreateUserController', () => {
-  test('should call validation with correct values', async () => {
+  test('should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(mockHttpRequest())
     expect(validateSpy).toHaveBeenCalledWith(mockedCreateUserParams)
   })
 
-  test('should return 400 if validation fails', async () => {
+  test('should return 400 if Validation fails', async () => {
     const { sut, validationStub } = makeSut()
     const mockedValidationFailureResponse = mockValidationFailure()
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(mockedValidationFailureResponse)
     const response = await sut.handle(mockHttpRequest())
     expect(response).toEqual(badRequest(mockedValidationFailureResponse.errors))
+  })
+
+  test('should call CreateUser with correct values', async () => {
+    const { sut, createUserStub } = makeSut()
+    const validateSpy = jest.spyOn(createUserStub, 'create')
+    await sut.handle(mockHttpRequest())
+    expect(validateSpy).toHaveBeenCalledWith(mockedCreateUserParams)
   })
 })
