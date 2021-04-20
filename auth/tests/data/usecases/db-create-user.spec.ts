8b8ebb07becbb07f5d972f93c5fc6a2a4d722442
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-20 14:38:54 -0300

    test: ensure DbCreateUser throws if Hasher throws

--- auth/tests/data/usecases/db-create-user.spec.ts
+++ auth/tests/data/usecases/db-create-user.spec.ts
@@ -1,6 +1,6 @@
 import { HasherSpy } from '@/tests/data/mocks/mock-cryptography'
-import { DbCreateUser } from '@/data/usecases'
 import { mockCreateUserParams } from '@/tests/domain/mocks'
+import { DbCreateUser } from '@/data/usecases'
 
 type SutTypes = {
   sut: DbCreateUser
@@ -22,5 +22,12 @@ describe('DbCreateUser', () => {
     const createUserParams = mockCreateUserParams()
     await sut.create(createUserParams)
     expect(hasherSpy.plaintext).toBe(createUserParams.password)
+  })
+
+  test('should throw if Hasher throws', async () => {
+    const { sut, hasherSpy } = makeSut()
+    jest.spyOn(hasherSpy, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.create(mockCreateUserParams())
+    await expect(promise).rejects.toThrow()
   })
 })
