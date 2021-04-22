bf12d750251b720b528851aa192c94e581299f42
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-22 16:01:39 -0300

    feat: ensure that LoginController returns 200 on success

--- auth/src/presentation/controllers/login-controller.ts
+++ auth/src/presentation/controllers/login-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest, serverError } from '@/presentation/helpers'
+import { badRequest, ok, serverError } from '@/presentation/helpers'
 import { Authentication } from '@/domain/usecases'
 
 export class LoginController implements Controller {
@@ -11,12 +11,12 @@ export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const authenticationParams = httpRequest.body
-      const result = this.validation.validate(authenticationParams.email)
-      if (result.code === 400) {
-        return badRequest(result.errors)
+      const validationResult = this.validation.validate(authenticationParams.email)
+      if (validationResult.code === 400) {
+        return badRequest(validationResult.errors)
       }
-      await this.authentication.auth(authenticationParams)
-      return Promise.resolve(null)
+      const authenticationResult = await this.authentication.auth(authenticationParams)
+      return ok(authenticationResult)
     } catch (error) {
       return serverError(error)
     }
