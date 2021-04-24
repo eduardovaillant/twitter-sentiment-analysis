b0a0a230f5d3e2d5f9982c7c3c74c918442f7dfc
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-24 16:25:06 -0300

    feat: ensure that RequiredFieldValidation returns 400 if validation fails

---
+++ auth/src/validation/validators/required-field-validation.ts
@@ -0,0 +1,16 @@
+import { Validation, ValidationResponse } from '@/presentation/protocols'
+
+export class RequiredFieldValidation implements Validation {
+  constructor (
+    private readonly fieldName: string
+  ) {}
+
+  validate (input: any): ValidationResponse {
+    if (!input[this.fieldName]) {
+      return {
+        code: 400,
+        errors: [`Missing param: '${this.fieldName}'`]
+      }
+    }
+  }
+}
