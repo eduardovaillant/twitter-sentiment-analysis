18c8209b99678842625f228421f4c4df86ee5636
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-24 16:25:19 -0300

    test: ensure that RequiredFieldValidation returns 400 if validation fails

---
+++ auth/tests/validation/validators/required-field-validation.spec.ts
@@ -0,0 +1,25 @@
+import { RequiredFieldValidation } from '@/validation/validators'
+
+import faker from 'faker'
+
+const field = faker.random.word()
+
+type SutTypes = {
+  sut: RequiredFieldValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new RequiredFieldValidation(field)
+  return {
+    sut
+  }
+}
+
+describe('RequiredFielValidation', () => {
+  test('should return 400 if validation fails', () => {
+    const { sut } = makeSut()
+    const result = sut.validate({ invalidField: faker.random.word() })
+    expect(result.code).toBe(400)
+    expect(result.errors[0]).toBe(`Missing param: '${field}'`)
+  })
+})
