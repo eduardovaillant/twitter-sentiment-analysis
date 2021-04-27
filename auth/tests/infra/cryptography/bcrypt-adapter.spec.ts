69f1305b9dcc4b0b178f7ee1085233e608cf612f
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-27 17:41:46 -0300

    test: ensure that BcryptAdapter throws if bcrypt.compare throws

--- auth/tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ auth/tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -60,5 +60,12 @@ describe('BcryptAdapter', () => {
       const isValid = await sut.compare('any_value', 'any_hash')
       expect(isValid).toBe(false)
     })
+
+    test('should throw if compare throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
