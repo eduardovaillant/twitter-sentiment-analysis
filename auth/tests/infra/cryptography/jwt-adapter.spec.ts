d9669e786942d5f20106b808453b31e4f37897fa
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-27 17:53:18 -0300

    test: ensure that JwtAdapter calls sign with correct values

---
+++ auth/tests/infra/cryptography/jwt-adapter.spec.ts
@@ -0,0 +1,22 @@
+import { JwtAdapter } from '@/infra/cryptography'
+
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return 'any_token'
+  }
+}))
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
+describe('JwtAdapter', () => {
+  test('should call sign with correct values', async () => {
+    const sut = makeSut()
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+})
