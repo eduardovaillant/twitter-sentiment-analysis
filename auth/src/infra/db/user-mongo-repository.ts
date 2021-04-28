ccf1fb1e169c5f5217f63c14185d98e30508f665
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-28 17:10:18 -0300

    feat: ensure that UserMongoRepository returns an user on loadByEmail success

--- auth/src/infra/db/user-mongo-repository.ts
+++ auth/src/infra/db/user-mongo-repository.ts
@@ -1,11 +1,25 @@
 import { MongoHelper } from '@/infra/db/mongo-helper'
-import { CreateUserRepository } from '@/data/protocols/db'
+import { CreateUserRepository, LoadUserByEmailRepository } from '@/data/protocols/db'
 import { CreateUserParams } from '@/domain/usecases'
+import { UserModel } from '@/domain/models'
 
-export class UserMongoRepository implements CreateUserRepository {
+export class UserMongoRepository implements CreateUserRepository, LoadUserByEmailRepository {
   async create (createUserParams: CreateUserParams): Promise<boolean> {
     const usersCollection = await MongoHelper.getCollection('users')
     const result = await usersCollection.insertOne(createUserParams)
     return result.ops[0] !== null
+  }
+
+  async loadByEmail (email: string): Promise<UserModel> {
+    const usersCollection = await MongoHelper.getCollection('users')
+    const user = await usersCollection.findOne({
+      email
+    }, {
+      projection: {
+        name: 1,
+        password: 1
+      }
+    })
+    return user && MongoHelper.map(user)
   }
 }
