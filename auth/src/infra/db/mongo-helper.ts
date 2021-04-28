01a321c86dc3ec9e871f6712d3f55a84a0a63648
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-28 17:47:57 -0300

    refactor: remove unnecessary method

--- auth/src/infra/db/mongo-helper.ts
+++ auth/src/infra/db/mongo-helper.ts
@@ -28,9 +28,5 @@ export const MongoHelper = {
   map: (data: any): any => {
     const { _id, ...rest } = data
     return { ...rest, id: _id }
-  },
-
-  mapCollection: (collection: any[]): any[] => {
-    return collection.map(c => MongoHelper.map(c))
   }
 }
