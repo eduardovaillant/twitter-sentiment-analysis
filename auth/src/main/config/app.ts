e83114a23c0bd485a4910191ee1954451c31a7b1
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-29 15:26:53 -0300

    feat: ensure that express use body-parser as json

--- auth/src/main/config/app.ts
+++ auth/src/main/config/app.ts
@@ -1,7 +1,9 @@
 import setupRoutes from './routes'
+import setupMiddlewares from './middlewares'
 
 import express from 'express'
 
 const app = express()
+setupMiddlewares(app)
 setupRoutes(app)
 export default app
