67fc4bdd7da6c2a6ce56fb6081edfc072d416d59
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-29 15:19:05 -0300

    feat: add express route adapter

---
+++ auth/src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@/presentation/protocols'
+
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {}),
+      ...(req.query || {})
+    }
+    const httpResponse = await controller.handle(request)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
