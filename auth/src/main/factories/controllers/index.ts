620d2decbbed6e8150684ffb27085773a614d7db
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-04-30 16:10:45 -0300

    feat: ensure that api/auth returns 200 on success

--- auth/src/main/factories/controllers/index.ts
+++ auth/src/main/factories/controllers/index.ts
@@ -1,1 +1,2 @@
 export * from './create-user-controller-factory'
+export * from './login-controller-factory'
--- auth/src/main/routes/user-routes.ts
+++ auth/src/main/routes/user-routes.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeCreateUserController } from '@/main/factories'
+import { makeCreateUserController, makeLoginController } from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateUserController()))
+  router.post('/auth', adaptRoute(makeLoginController()))
 }
