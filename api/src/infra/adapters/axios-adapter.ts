a55e9382b7536a2373ff53f1533f15ffadd9e936
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-01 14:10:43 -0300

    feat: add axios adapter tests

---
+++ api/src/infra/adapters/axios-adapter.ts
@@ -0,0 +1,15 @@
+import { HttpPost } from '@/data/protocols/adapters'
+
+import axios, { AxiosResponse } from 'axios'
+
+export class AxiosAdapter implements HttpPost {
+  async post (url: string, data: any, config?: any): Promise<any> {
+    let response: AxiosResponse<any>
+    if (config) {
+      response = await axios.post(url, data, config)
+    } else {
+      response = await axios.post(url, data)
+    }
+    return response
+  }
+}
