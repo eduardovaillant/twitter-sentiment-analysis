ae545f67b7a3ab659a3afc4cbaf47d4fbb8d725b
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-01 14:26:46 -0300

    feat: add TwitterClient

---
+++ api/src/infra/clients/twitter-client.ts
@@ -0,0 +1,32 @@
+
+import { HttpPost, TwitterAddRule, TwitterAddRuleResponse } from '@/data/protocols/adapters'
+import { AddRuleModel } from '@/domain/usecases'
+
+import env from '@/main/config/env'
+
+// TODO return an error if the api call is not successfull
+// TODO error handling, twitter api responses to the user, validation
+export class TwitterClient implements TwitterAddRule {
+  constructor (
+    private readonly httpPost: HttpPost
+  ) {}
+
+  async addRule (rule: AddRuleModel): Promise<TwitterAddRuleResponse> {
+    const config = { headers: { Authorization: `Bearer ${env.bearerToken}` } }
+    const body = {
+      add: [
+        {
+          value: rule.value,
+          tag: rule.tag || ''
+        }
+      ]
+    }
+    const result = await this.httpPost.post(env.baseUrl + 'tweets/search/stream/rules', body, config)
+    const twitterAddRuleResponse: TwitterAddRuleResponse = {
+      twitter_rule_id: result.data.data[0].id,
+      value: result.data.data[0].value,
+      tag: result.data.data[0].tag
+    }
+    return twitterAddRuleResponse
+  }
+}
--- api/src/main/config/env.ts
+++ api/src/main/config/env.ts
@@ -1,5 +1,11 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/auth',
   port: process.env.PORT || 3010,
-  jwtSecret: process.env.JWT_SECRET || 'tj67O==5H'
+  jwtSecret: process.env.JWT_SECRET || 'tj67O==5H',
+  baseUrl: process.env.BASE_URL || '',
+  apiKey: process.env.API_KEY || '',
+  apiSecret: process.env.API_SECRET || '',
+  bearerToken: process.env.BEARER_TOKEN || '',
+  accessToken: process.env.ACCESS_TOKEN || '',
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || ''
 }
